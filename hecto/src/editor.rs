c9f7e22fc2c2d82485aa26990f9db06644c8eeb2
jarinosuke(Naoki ISHIKAWA) <naokiishikawa0808@gmail.com>
2021-02-20 10:50:57 +0900

    separate read method

--- hecto/src/editor.rs
+++ hecto/src/editor.rs
@@ -6,28 +6,35 @@ use termion::raw::IntoRawMode;
 pub struct Editor {}
 
 impl Editor {
-    pub fn default() -> Self {
-        Self {}
-    }
 
     pub fn run(&self) {
         let _stdout = stdout().into_raw_mode().unwrap();
 
-        for key in io::stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Char(c) => {
-                        if c.is_control() {
-                            println!("{:?} \r", c as u8);
-                        } else {
-                            println!("{:?} ({})\r", c as u8, c);
-                        }
-                    }
-                    Key::Ctrl('q') => break,
-                    _ => println!("{:?}\r", key),
-                }
-                Err(e) => die(e),
+        loop {
+            if let Err(e) = self.process_keypress() {
+                die(e);
             }
+        }
+    }
+
+    pub fn default() -> Self {
+        Self {}
+    }
+
+    fn process_keypress(&self) -> Result<(), std::io::Error> {
+        let pressed_key = read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => panic!("Program end"),
+            _ => (),
+        }
+        Ok(())
+    }
+}
+
+fn read_key() -> Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
         }
     }
 }
