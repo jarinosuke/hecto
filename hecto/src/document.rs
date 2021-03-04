526bed1c7b0c059142ca1231f158f4f491f2c9c7
jarinosuke(Naoki ISHIKAWA) <naokiishikawa0808@gmail.com>
2021-03-05 08:11:11 +0900

    insert characters

--- hecto/src/document.rs
+++ hecto/src/document.rs
@@ -1,3 +1,4 @@
+use crate::Position;
 use crate::Row;
 use std::fs;
 
@@ -8,7 +9,7 @@ pub struct Document {
 }
 
 impl Document {
-  pub fn open(filename: &str) -> Result<Self, std::io::Error> {
+    pub fn open(filename: &str) -> Result<Self, std::io::Error> {
         let contents = fs::read_to_string(filename)?;
         let mut rows = Vec::new();
         for value in contents.lines() {
@@ -27,5 +28,15 @@ impl Document {
     }
     pub fn len(&self) -> usize {
         self.rows.len()
+    }
+    pub fn insert(&mut self, at: &Position, c: char) {
+        if at.y == self.len() {
+            let mut row = Row::default();
+            row.insert(0, c);
+            self.rows.push(row);
+        } else if at.y < self.len() {
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.insert(at.x, c);
+        }
     }
 }
